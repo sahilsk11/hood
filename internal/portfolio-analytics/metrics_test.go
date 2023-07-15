0d41f7cf4b90de884a1c8490428b1b1af766c601
sahilsk11 <sahilkapur.a@gmail.com>
2023-07-15 14:23:22 -0400

    stock split returns test

--- internal/portfolio-analytics/metrics_test.go
+++ internal/portfolio-analytics/metrics_test.go
@@ -58,6 +58,32 @@ func TestCalculateNetRealizedReturns(t *testing.T) {
 		require.True(t, out.Equal(decimal.NewFromFloat(0.2)))
 	})
 
+	t.Run("stock split", func(t *testing.T) {
+		tx, err := dbConn.Begin()
+		require.NoError(t, err)
+		db.RollbackAfterTest(t, tx)
+
+		buy := newTrade(400, 1, model.TradeActionType_Buy)
+		_, _, err = tiService.ProcessTdaBuyOrder(ctx, tx, buy, 0)
+		require.NoError(t, err)
+
+		_, _, err = tiService.AddAssetSplit(ctx, tx, model.AssetSplit{
+			Symbol:    "AAPL",
+			Ratio:     4,
+			Date:      time.Now(),
+			CreatedAt: time.Now(),
+		})
+		require.NoError(t, err)
+
+		sell := newTrade(110, 4, model.TradeActionType_Sell)
+		_, _, err = tiService.ProcessSellOrder(ctx, tx, sell)
+		require.NoError(t, err)
+
+		out, err := CalculateNetRealizedReturns(tx)
+		require.NoError(t, err)
+		require.True(t, out.Equal(decimal.NewFromFloat(0.1)))
+	})
+
 }
 
 func TestCalculateNetUnrealizedReturns(t *testing.T) {
