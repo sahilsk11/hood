646ee14dd87ca7ba5181d6c2c41e96707ce5f938
sahilsk11 <sahilkapur.a@gmail.com>
2023-07-26 02:23:43 -0400

    .

--- internal/domain/portfolio.go
+++ internal/domain/portfolio.go
@@ -10,5 +10,17 @@ type Portfolio struct {
 	OpenLots   map[string][]*OpenLot
 	ClosedLots map[string][]ClosedLot
 	Cash       decimal.Decimal
-	Date       time.Time
+	LastAction time.Time
+}
+
+func (p Portfolio) GetOpenLotSymbols() []string {
+	symbolsMap := map[string]struct{}{}
+	for s := range p.OpenLots {
+		symbolsMap[s] = struct{}{}
+	}
+	symbols := []string{}
+	for s := range symbolsMap {
+		symbols = append(symbols, s)
+	}
+	return symbols
 }
