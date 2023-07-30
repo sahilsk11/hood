5d410f96f4c52a2e9f946b93238c3877643d194a
sahilsk11 <sahilkapur.a@gmail.com>
2023-07-30 14:38:58 -0400

    .

--- internal/metrics/risk.go
+++ internal/metrics/risk.go
@@ -6,6 +6,7 @@ import (
 	"hood/internal/db/models/postgres/public/model"
 	db "hood/internal/db/query"
 	. "hood/internal/domain"
+	"math"
 	"sort"
 	"time"
 
@@ -23,12 +24,67 @@ func DailyStdevOfAsset(tx *sql.Tx, symbol string) (float64, error) {
 		return 0, err
 	}
 
-	changes, err := pricesListToMappedChanges(prices)
+	changes := pricesListToMappedChanges(prices)
+	data := decListToFloat64(changes)
+	return stats.StandardDeviation(data)
+}
+
+func DailyStdevOfPortfolio(tx *sql.Tx, p Portfolio) (float64, error) {
+	symbols := p.GetOpenLotSymbols()
+	mappedStdev := map[string]decimal.Decimal{}
+	for _, symbol := range symbols {
+		stdev, err := DailyStdevOfAsset(tx, symbol)
+		if err != nil {
+			return 0, err
+		}
+		mappedStdev[symbol] = decimal.NewFromFloat(stdev)
+	}
+	start := time.Now().Add(-1 * stdevRange)
+	covariances, err := covariances(tx, symbols, start)
 	if err != nil {
 		return 0, err
 	}
-	data := decListToFloat64(changes[symbol])
-	return stats.StandardDeviation(data)
+	weights, err := assetWeights(tx, p)
+	if err != nil {
+		return 0, err
+	}
+
+	// idk, used a lot
+	two := decimal.NewFromInt(2)
+
+	squaredTerms := decimal.Zero
+	for _, s := range symbols {
+		t := (weights[s].Pow(two)).Mul(mappedStdev[s].Pow(two))
+		squaredTerms = squaredTerms.Add(t)
+	}
+
+	covarianceTerms := []decimal.Decimal{}
+	sort.Strings(symbols)
+	for i := range symbols {
+		s1 := symbols[i]
+		s1Weight := weights[s1]
+		s1Stdev := mappedStdev[s1]
+		for j := i + 1; j < len(symbols); j++ {
+			s2 := symbols[j]
+			s2Weight := weights[s2]
+			s2Stdev := mappedStdev[s2]
+			covariance := decimal.NewFromFloat(covariances[s1+"-"+s2])
+			t := two.Mul(s1Weight).Mul(s2Weight).Mul(covariance).Mul(s1Stdev).Mul(s2Stdev)
+			covarianceTerms = append(covarianceTerms, t)
+		}
+	}
+	expectedCovarianceTerms := (len(symbols)*len(symbols) - 1) / 2
+	if len(covarianceTerms) != expectedCovarianceTerms {
+		return 0, fmt.Errorf("expected %d covariance terms, calculated %d", expectedCovarianceTerms, len(covarianceTerms))
+	}
+	covarianceTermsSum := decimal.Zero
+	for _, c := range covarianceTerms {
+		covarianceTermsSum = covarianceTermsSum.Add(c)
+	}
+
+	x := math.Sqrt((squaredTerms.Add(covarianceTermsSum)).InexactFloat64())
+
+	return x, nil
 }
 
 func assetWeights(tx *sql.Tx, p Portfolio) (map[string]decimal.Decimal, error) {
@@ -42,6 +98,9 @@ func assetWeights(tx *sql.Tx, p Portfolio) (map[string]decimal.Decimal, error) {
 	if err != nil {
 		return nil, err
 	}
+	if value.Equal(decimal.Zero) {
+		return nil, fmt.Errorf("portfolio has 0 net value")
+	}
 	weights := map[string]decimal.Decimal{}
 	for symbol := range p.OpenLots {
 		positionValue := p.GetQuantity(symbol).Mul(priceMap[symbol])
@@ -52,20 +111,15 @@ func assetWeights(tx *sql.Tx, p Portfolio) (map[string]decimal.Decimal, error) {
 }
 
 // assume prices is sorted by date
-func pricesListToMappedChanges(prices []model.Price) (map[string][]decimal.Decimal, error) {
-	mappedPriceLists := map[string][]decimal.Decimal{}
+func pricesListToMappedChanges(prices []model.Price) []decimal.Decimal {
+	mappedPriceLists := []decimal.Decimal{}
 	for i, p := range prices[1:] {
-		s := p.Symbol
 		prevPrice := prices[i].Price
-		changes, ok := mappedPriceLists[s]
-		if !ok {
-			mappedPriceLists[s] = []decimal.Decimal{}
-		}
 		percentChange := ((p.Price.Sub(prevPrice)).Div(prevPrice)).Mul(decimal.NewFromInt(100))
-		fmt.Printf("%s %f-%f/%f = %f\n", p.Date.Format("2006-01-02"), p.Price.InexactFloat64(), prevPrice.InexactFloat64(), prevPrice.InexactFloat64(), percentChange.InexactFloat64())
-		mappedPriceLists[s] = append(changes, percentChange)
+		// fmt.Printf("%s %f-%f/%f = %f\n", p.Date.Format("2006-01-02"), p.Price.InexactFloat64(), prevPrice.InexactFloat64(), prevPrice.InexactFloat64(), percentChange.InexactFloat64())
+		mappedPriceLists = append(mappedPriceLists, percentChange)
 	}
-	return mappedPriceLists, nil
+	return mappedPriceLists
 }
 
 func covariances(tx *sql.Tx, symbols []string, start time.Time) (map[string]float64, error) {
@@ -74,23 +128,35 @@ func covariances(tx *sql.Tx, symbols []string, start time.Time) (map[string]floa
 	if err != nil {
 		return nil, err
 	}
-	priceChangeMap, err := pricesListToMappedChanges(prices)
-	if err != nil {
-		return nil, err
+	pricesBySymbol := map[string][]model.Price{}
+	priceDates := map[string]map[string]struct{}{}
+
+	for _, p := range prices {
+		if _, ok := pricesBySymbol[p.Symbol]; !ok {
+			pricesBySymbol[p.Symbol] = []model.Price{}
+		}
+		pricesBySymbol[p.Symbol] = append(pricesBySymbol[p.Symbol], p)
+		if _, ok := priceDates[p.Symbol]; !ok {
+			priceDates[p.Symbol] = map[string]struct{}{}
+		}
+		priceDates[p.Symbol][p.Date.Format("2006-01-02")] = struct{}{}
 	}
 
 	sort.Strings(symbols)
 	for i := range symbols {
-		s1 := priceChangeMap[symbols[i]]
-		for j := i; j < len(symbols); j++ {
-			s2 := priceChangeMap[symbols[j]]
-			if len(s1) != len(s2) {
-				return nil, fmt.Errorf("inconsistent price days: %d for %s and %d for %s", len(s1), symbols[i], len(s2), symbols[j])
+		s1 := symbols[i]
+		s1PriceChanges := pricesListToMappedChanges(pricesBySymbol[s1])
+		for j := i + 1; j < len(symbols); j++ {
+			s2 := symbols[j]
+			s2PriceChanges := pricesListToMappedChanges(pricesBySymbol[s2])
+			setDiff := setDifference(priceDates[s1], priceDates[s2])
+			if len(s1PriceChanges) != len(s2PriceChanges) {
+				return nil, fmt.Errorf("inconsistent price days: %d for %s and %d for %s: %v", len(s1PriceChanges), symbols[i], len(s2PriceChanges), symbols[j], setDiff)
 			}
 			// https://www.investopedia.com/terms/c/covariance.asp
 			c, err := stats.Covariance(
-				decListToFloat64(s1),
-				decListToFloat64(s2),
+				decListToFloat64(s1PriceChanges),
+				decListToFloat64(s2PriceChanges),
 			)
 			if err != nil {
 				return nil, err
@@ -109,4 +175,23 @@ func decListToFloat64(data []decimal.Decimal) stats.Float64Data {
 		out = append(out, d.InexactFloat64())
 	}
 	return out
+}
+
+func setDifference(s1, s2 map[string]struct{}) []string {
+	diff := make([]string, 0)
+	for key := range s1 {
+		if _, found := s2[key]; !found {
+			diff = append(diff, key)
+		}
+	}
+	for key := range s2 {
+		if _, found := s1[key]; !found {
+			diff = append(diff, key)
+		}
+	}
+	sort.Strings(diff)
+	if len(s1) != len(s2) && len(diff) == 0 {
+		panic("mismatched len but could not find diff")
+	}
+	return diff
 }
--- internal/metrics/risk_test.go
+++ internal/metrics/risk_test.go
@@ -1,1 +1,148 @@
 package metrics
+
+import (
+	"fmt"
+	db "hood/internal/db/query"
+	. "hood/internal/domain"
+	"reflect"
+	"testing"
+
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDailyStdevOfPortfolio(t *testing.T) {
+	t.Run("SPY", func(t *testing.T) {
+		dbConn, err := db.New()
+		require.NoError(t, err)
+		tx, err := dbConn.Begin()
+		require.NoError(t, err)
+		db.RollbackAfterTest(t, tx)
+
+		portfolio := Portfolio{
+			OpenLots: map[string][]*OpenLot{
+				"SPY": {
+					{
+						Quantity: decimal.NewFromInt(10),
+					},
+				},
+			},
+			ClosedLots: map[string][]ClosedLot{},
+		}
+		stdev, err := DailyStdevOfPortfolio(tx, portfolio)
+		require.NoError(t, err)
+
+		fmt.Println(stdev)
+		t.Fail()
+	})
+
+	t.Run("multi-asset", func(t *testing.T) {
+		dbConn, err := db.New()
+		require.NoError(t, err)
+		tx, err := dbConn.Begin()
+		require.NoError(t, err)
+		db.RollbackAfterTest(t, tx)
+
+		portfolio := Portfolio{
+			OpenLots: map[string][]*OpenLot{
+				"SPY": {
+					{
+						Quantity: decimal.NewFromInt(10),
+					},
+				},
+				"AAPL": {
+					{
+						Quantity: decimal.NewFromInt(10),
+					},
+				},
+			},
+			ClosedLots: map[string][]ClosedLot{},
+		}
+		stdev, err := DailyStdevOfPortfolio(tx, portfolio)
+		require.NoError(t, err)
+
+		fmt.Println(stdev)
+		t.Fail()
+	})
+}
+
+func Test_setDifference(t *testing.T) {
+	type args struct {
+		s1 map[string]struct{}
+		s2 map[string]struct{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "second set empty",
+			args: args{
+				s1: map[string]struct{}{"AAPL": {}, "DIS": {}},
+				s2: map[string]struct{}{},
+			},
+			want: []string{"AAPL", "DIS"},
+		},
+		{
+			name: "first set empty",
+			args: args{
+				s1: map[string]struct{}{},
+				s2: map[string]struct{}{"AAPL": {}, "DIS": {}},
+			},
+			want: []string{"AAPL", "DIS"},
+		},
+		{
+			name: "some overlap",
+			args: args{
+				s1: map[string]struct{}{"AAPL": {}},
+				s2: map[string]struct{}{"AAPL": {}, "DIS": {}},
+			},
+			want: []string{"DIS"},
+		},
+		{
+			name: "no difference",
+			args: args{
+				s1: map[string]struct{}{"AAPL": {}, "DIS": {}},
+				s2: map[string]struct{}{"AAPL": {}, "DIS": {}},
+			},
+			want: []string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := setDifference(tt.args.s1, tt.args.s2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("setDifference() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDailyStdevOfAsset(t *testing.T) {
+	t.Run("SPY", func(t *testing.T) {
+		dbConn, err := db.New()
+		require.NoError(t, err)
+		tx, err := dbConn.Begin()
+		require.NoError(t, err)
+		db.RollbackAfterTest(t, tx)
+
+		stdev, err := DailyStdevOfAsset(tx, "SPY")
+		require.NoError(t, err)
+
+		fmt.Println(stdev)
+		t.Fail()
+	})
+	t.Run("AAPL", func(t *testing.T) {
+		dbConn, err := db.New()
+		require.NoError(t, err)
+		tx, err := dbConn.Begin()
+		require.NoError(t, err)
+		db.RollbackAfterTest(t, tx)
+
+		stdev, err := DailyStdevOfAsset(tx, "AAPL")
+		require.NoError(t, err)
+
+		fmt.Println(stdev)
+		t.Fail()
+	})
+}
